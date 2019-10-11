352f257a6dfc0835e34504006c3019010ad3cc68
Kyle Cole <kylepcole@gmail.com>
2019-10-11 23:10:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array)
-  array.insert 
+def using_insert(array, item)
+  array.insert(item)
 end
 
 def using_uniq(array)
