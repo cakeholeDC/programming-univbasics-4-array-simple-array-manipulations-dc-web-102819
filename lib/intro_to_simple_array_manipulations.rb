767782e93d81d72c86434a234d5717f61b724a7f
Kyle Cole <kylepcole@gmail.com>
2019-10-11 23:07:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,4 +23,17 @@ def shift_with_args(array)
 end
 
 def using_concat(array1, array2)
-  array1 + array2
+  array1.concat(array2)
+end
+
+def using_insert(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
